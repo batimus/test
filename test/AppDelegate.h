3554354523b9ce147cfe6cbbcd0ea790414df9e6
Irakli Tsagareli <kern@Macs-MacBook-Pro.local>
2015-08-17 10:46:06 +0400

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Irakli Tsagareli on 8/17/15.
+//  Copyright (c) 2015 Irakli Tsagareli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Irakli Tsagareli on 8/17/15.
+//  Copyright (c) 2015 Irakli Tsagareli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
